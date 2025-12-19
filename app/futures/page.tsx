ab6c680665ce3d8b277e0ba06b76cf2f045614b9
Aditya Patnaik <patnaikaditya@gmail.com>
2025-12-19 19:33:46 +0530

    updated links

---
+++ app/futures/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-800">
+        🚧 Under Construction
+      </h1>
+    </div>
+  );
+};
+
+export default page;
